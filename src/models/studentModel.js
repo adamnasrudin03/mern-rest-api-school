9d3d23504ef6675c8dbbdc5583692185e91662a9
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-11-24 13:21:41 +0700

    setup model student

---
+++ src/models/studentModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Student = new Schema(
+  {
+    npm: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    class: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Student", Student);
