4b69cfea2385c5d73b1cda2ecd44659ff1796268
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-11-25 14:30:55 +0700

    new route lesson and teacher

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const mongoose = require("mongoose");
 const app = express();
 const router = express.Router();
 
-
 const studentRouter = require("./src/routers/studentRouter");
-// const teacherRouter = require("./src/routers/teacherRouter");
-// const lessonRouter = require("./src/routers/lessonRouter");
-// const scoreRouter = require("./src/routers/scoreRouter");
-
-
+const teacherRouter = require("./src/routers/teacherRouter");
+const lessonRouter = require("./src/routers/lessonRouter");
 
 //body parser req json type
 app.use(bodyParser.json());
@@ -21,16 +17,18 @@ const PORT = process.env.PORT || 8082;
 mongoose
   .connect("mongodb://localhost:27017/")
   .then(() => {
-    app.listen(PORT, () => 
-    console.log(`Connection Success", Server is running on port ${PORT}.`));
+    app.listen(PORT, () =>
+      console.log(`Connection Success", Server is running on port ${PORT}.`)
+    );
   })
-  .catch((err) => console.log(err));
-
+  .catch((err) => {
+    console.log("Error name : ", err.name);
+    console.log("Error messages : ", err.message);
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to web application." });
 });
-
 
 //Access CROS
 app.use((req, res, next) => {
@@ -45,9 +43,8 @@ app.use((req, res, next) => {
 
 //Route
 app.use("/students", studentRouter);
-// app.use("/teachers", teacherRouter);
-// app.use("/lessons", lessonRouter);
-// app.use("/assessments", scoreRouter);
+app.use("/teachers", teacherRouter);
+app.use("/lessons", lessonRouter);
 
 //Handle error page not found
 app.use(function (req, res, next) {
