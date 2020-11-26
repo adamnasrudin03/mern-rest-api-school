a841a5290889bf8827d209263477717114b0b92f
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-11-26 16:14:36 +0700

    Implement jwt all route

--- src/routers/teacherRouter.js
+++ src/routers/teacherRouter.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { authJwt } = require("../middlewares");
 const controller = require("../controllers/teacherController");
 
-router.post("/add", controller.addOne);
-router.get("/", controller.findAll);
-router.get("/:id", controller.findById);
-router.put("/update/:id", controller.updateById);
-router.delete("/delete/:id", controller.deleteById);
+router.post("/add", [authJwt.verifyToken], controller.addOne);
+router.get("/", [authJwt.verifyToken], controller.findAll);
+router.get("/:id", [authJwt.verifyToken], controller.findById);
+router.put("/update/:id", [authJwt.verifyToken], controller.updateById);
+router.delete(
+  "/delete/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteById
+);
 
 module.exports = router;
