92fce9fc2d6ed38dfe8bf41bf17879b2a0fd131f
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-11-26 21:37:25 +0700

    change route and express-validator

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,13 +1,26 @@
 const { verifySignUp } = require("../middlewares");
+const { body } = require("express-validator");
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/authController");
 
 router.post(
-  "/auth/signup",
-  [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+  "/signup",
+  [
+    verifySignUp.checkDuplicateUsernameOrEmail,
+    verifySignUp.checkRolesExisted,
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password min 8 characters"),
+    body("passwordConfirmation").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+      return true;
+    }),
+  ],
   controller.signup
 );
-router.post("/auth/signin", controller.signin);
+router.post("/signin", controller.signin);
 
 module.exports = router;
